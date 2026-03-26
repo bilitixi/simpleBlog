b65d16ce9250ec077db0db149ec82b31d62f22df
bilitixi <bilitixi@gmail.com>
2026-03-26 19:31:52 +1300

    deploy

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from blog.views import index, category, post, create_category, PostListView, PostDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-]
+    path('', index, name='index'),
+    path('category/<int:category_id>/', category, name='category'),
+    #path('post/<int:post_id>/', post, name='post'),
+    path('create_category/', create_category, name='create_category'),
+
+    path('post_list/', PostListView.as_view(), name='post_list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail')]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
 
-from blog.models import Category
+
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
+
+from blog.models import Category, Post
 
 
 # Create your views here.
 def index(request):
     categories = Category.objects.all()
     return render(request, 'blog/index.html', {'categories': categories})
+def category(request, category_id):
+    category = Category.objects.get(id=category_id)
+    return render(request, 'blog/category.html', {'category': category})
+def post(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, 'blog/post.html', {'post': post})
+def create_category(request):
+    if request.method == 'POST':
+        name = request.POST['category_name']
+        Category.objects.create(name=name)
+        return redirect('category', category_id = Category.objects.last().id)
+    return render(request, 'blog/create_category.html')
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
